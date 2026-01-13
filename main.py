bb64a83d5be0b45447fdec2cc9461a84890d37fb
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-13 11:33:47 +0900

    매개변수화

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 
-tag_name = "h1"
-angle_bracket = ["<",">"]
 content = "블루윈"
 
 
 #열기/닫기 -> 이분법 코드 생각
-def make_tag(isOpen):
+def make_tag(isOpen,tag_name):
+    angle_bracket = ["<",">"]
+    slash = "/"
+
     if isOpen == True:
         open = angle_bracket[0] + tag_name + angle_bracket[1]
         return open
     else:
-        close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
+        close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
         return close
     
 
