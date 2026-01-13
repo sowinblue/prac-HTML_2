5a74f4ea3f5b7bc2798cfd75d59159f173f34db3
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-13 11:31:00 +0900

    코드 정리

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ angle_bracket = ["<",">"]
 content = "블루윈"
 
 
-tag_jolib_open = angle_bracket[0] + tag_name + angle_bracket[1]
-tag_jolib_close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
-
-
 #열기/닫기 -> 이분법 코드 생각
 def make_tag(isOpen):
     if isOpen == True:
-        open = tag_jolib_open
+        open = angle_bracket[0] + tag_name + angle_bracket[1]
         return open
     else:
-        close = tag_jolib_close
+        close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
         return close
     
 
